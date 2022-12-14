d78b204c5e99afaaa42c5575908b1faa78697fda
Sean Wallace <sean.wallace.australia@gmail.com>
2022-12-15 09:00:12 +1100

    solved problem with: heroku config:set NPM_CONFIG_PRODUCTION=false, https://stackoverflow.com/questions/73693910/webpack-not-found-when-deploying-my-app-to-heroku

--- dep.sh
+++ dep.sh
@@ -2,7 +2,7 @@
 # use chmod +x fname.sh to give permissions
 # run ./fname.sh to run script
 git add .
-git commit -m 'heroku trial and error to fix webpack: not found'
+git commit -m 'solved problem with: heroku config:set NPM_CONFIG_PRODUCTION=false, https://stackoverflow.com/questions/73693910/webpack-not-found-when-deploying-my-app-to-heroku'
 git push
 # next line auto done by line above
 # git push heroku main 
