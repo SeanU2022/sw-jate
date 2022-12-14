7d8c4c85db58f3b9cffdd4946ceaa4770b1c224a
Sean Wallace <sean.wallace.australia@gmail.com>
2022-12-15 06:50:01 +1100

    remove package.json heroku postbuild

--- dep.sh
+++ dep.sh
@@ -1,3 +1,6 @@
+# use touch fname.sh to create
+# use chmod +x fname.sh to give permissions
+# run ./fname.sh to run script
 git add .
 git commit -m 'heroku trial and error to fix webpack: not found'
 git push
