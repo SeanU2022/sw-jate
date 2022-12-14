6a447b02e308bec4342b84f9c5cd434db96642b0
Sean Wallace <sean.wallace.australia@gmail.com>
2022-12-15 07:36:40 +1100

    add webpack.config>module>rules>use>loader>plugins[@babel...]

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -105,7 +105,8 @@ module.exports = () => {
           use: {
             loader: "babel-loader",
             options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             }
           }
         }
